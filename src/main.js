6664cf3cf79698812be9ce0ca28d1ae92ead2ceb
dannytee <tundeojediran@gmail.com>
2022-06-27 22:13:53 +0100

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faGripVertical } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faGripVertical)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
